aa721ac3bd1e009ae2acb73e2a7c210db78f3f32
Abhishek <98501642+AbhishekDuddupudi@users.noreply.github.com>
2025-02-11 05:22:38 +0000

    Unit tests for Intermediate calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,27 +1,19 @@
-'''My Calculator Test'''
 import pytest
-from calculator import add, subtract, multiply, divide
-
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
-
-def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+from calculator import Calculator
 
 def test_add():
-    assert add(2, 3) == 5
+    assert Calculator.add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(5, 3) == 2
+    assert Calculator.subtract(5, 3) == 2
 
 def test_multiply():
-    assert multiply(4, 3) == 12
+    assert Calculator.multiply(4, 3) == 12
 
 def test_divide():
-    assert divide(10, 2) == 5
+    assert Calculator.divide(10, 2) == 5
 
 def test_divide_by_zero():
+    """Ensure ValueError is raised for division by zero."""
     with pytest.raises(ValueError):
-        divide(10, 0)
+        Calculator.divide(10, 0)
