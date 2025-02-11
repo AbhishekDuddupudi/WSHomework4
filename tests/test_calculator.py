bd779c2ab26d452b3dbdc5a10ce982a1755aa3a9
Abhishek <98501642+AbhishekDuddupudi@users.noreply.github.com>
2025-02-11 05:05:39 +0000

    tests to check calculator operations

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,6 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+import pytest
+from calculator import add, subtract, multiply, divide
 
 def test_addition():
     '''Test that addition function works '''    
@@ -8,3 +9,19 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(2,2) == 0
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
